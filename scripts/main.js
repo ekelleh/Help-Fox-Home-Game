2d8ad27f0afe5ee2570d8590f822556d4d895372
Helleke Heikkine <helleke.heikkinen@gmail.com>
2019-04-01 13:58:42 +0200

    interface changes

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,8 @@ import choiceData from "./choice-data.js";
 
 const beginning = new Story(storyData["0A"].content, "0A");
 const choices = [];
+let currentStory = beginning;
+renderEverything();
 
 const createChoices = function() {
     choiceData.forEach(function(choice) {
@@ -24,3 +26,31 @@ function getChoicesForCurrentStory(story) {
 }
 
 console.log(getChoicesForCurrentStory(beginning));
+
+function renderStory() {
+    let storyBox = document.querySelector(".story-box");
+    storyBox.innerHTML = currentStory.content;
+}
+
+console.log(document.querySelector(".story-box p"));
+console.log(currentStory.content);
+console.log(renderStory());
+
+function renderChoices() {
+    let choiceBox = document.querySelector;
+}
+
+function renderEverything() {
+    renderStory();
+    renderChoices();
+}
+
+// tee funktio render joka asettaa nykyisen storyn ja nykyiset choicet HTMLään , ktso pizzat
+// render story ja render choices fuktiot jota kutsutaan
+// choicea klikkaamalla vaihtuu nykyinen story ja kutsuu render funtkiota
+
+/**
+ Haluat, että `renderStory` funktio laittaa sen `currentStory`n contentin sinne sivulla olevaan 
+ diviin, jossa se nykyisen storyn teksti näkyy
+Ja sit `renderChoices` tekee saman, mutta niille Choice napeille
+ */
